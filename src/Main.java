ff14c29c253060511b362f8c397248cb09764ebb
Anindo Paul <anindopaul293@gmail.com>
2026-09-22 11:59:11 +0600

    Implement basic HTTP server with file handling

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        String envPort = System.getenv("PORT");
+        if (envPort != null) {
+            port = Integer.parseInt(envPort);
+        }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        
+        server.createContext("/", new StaticFileHandler());
+        server.createContext("/api/status", new StatusHandler());
+        
+        server.setExecutor(null); 
+        System.out.println("Server running on port: " + port);
+        server.start();
+    }
+
+    static class StaticFileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            File file = new File("index.html");
+
+            if (file.exists()) {
+                byte[] bytearray = new byte[(int) file.length()];
+                FileInputStream fis = new FileInputStream(file);
+                fis.read(bytearray);
+                fis.close();
+
+                exchange.getResponseHeaders().add("Content-Type", "text/html");
+                exchange.sendResponseHeaders(200, file.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(bytearray);
+                os.close();
+            } else {
+                String response = "404 - index.html file missing!";
+                exchange.sendResponseHeaders(404, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
+    }
+
+    static class StatusHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+
+            String response = "{\"status\":\"online\", \"message\":\"Java Backend Connected!\"}";
+            exchange.sendResponseHeaders(200, response.length());
+            
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
